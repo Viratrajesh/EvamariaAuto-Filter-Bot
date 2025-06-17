b4a8cf264d71de30799ad1f87a75212e1ee55d61
Muhammedshifin <muhammedshifink4@gmail.com>
2025-06-17 20:33:14 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,14 +1,19 @@
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
+from info import ADMINS, REQ_CHANNEL
 from database.users_chats_db import db
-from info import ADMINS, AUTH_CHANNEL
 
-@Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
-async def join_reqs(client, message: ChatJoinRequest):
-  if not await db.find_join_req(message.from_user.id):
-    await db.add_join_req(message.from_user.id)
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL))
+async def join_reqs(client, join_req: ChatJoinRequest):
+    # print(f"Request: {join_req.from_user.id}")
+    
+    try:
+        user_id = join_req.from_user.id
 
-@Client.on_message(filters.command("delreq") & filters.private & filters.user(ADMINS))
-async def del_requests(client, message):
-    await db.del_join_req()    
-    await message.reply("<b>⚙ ꜱᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ᴄʜᴀɴɴᴇʟ ʟᴇғᴛ ᴜꜱᴇʀꜱ ᴅᴇʟᴇᴛᴇᴅ</b>")
+        requested = await db.get_req(user_id) 
+        if requested:
+            return
+        
+        await db.add_req(user_id=user_id)
+    except Exception as e:
+        print(e)
