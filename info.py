77b028f0683b82fb8b6796671b8b169668603659
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-06-22 18:23:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1002896124865')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 'https://t.me/Askmovieslink1'))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 'Askmovieslink1'))
 
 # bot settings
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
